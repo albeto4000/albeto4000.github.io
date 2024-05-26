aaffec42519f607d1a69b6f381d1fdf89ddaa09d
Matt Dolin <mgd324@lehigh.edu>
2024-05-26 00:21:51 -0400

    First commit

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	
+	$("#home").css("height", window.innerHeight);
+});
+
+$(".card").on("mouseover", function(){
+	$(this).removeClass("border-secondary");
+	$(this).addClass("border-success");
+});
+
+$(".card").on("mouseout", function(){
+	$(this).removeClass("border-success");
+	$(this).addClass("border-secondary");
+});
+
+$("#contact a").on("mouseover", function(){
+	$(this).removeClass("text-dark");
+	$(this).addClass("text-success");
+});
+
+$("#contact a").on("mouseout", function(){
+	$(this).removeClass("text-success");
+	$(this).addClass("text-secondary");
+});
+
+$(document).on("scroll", function(){
+	if(window.scrollY < $("#portfolio").offset().top - 100 && window.scrollY > $("#about").offset().top - 100){
+		$(".nav-item").removeClass("active");
+		$("#aboutLink").addClass("active");
+	}
+	else if(window.scrollY >= $("#portfolio").offset().top - 100){
+		$(".nav-item").removeClass("active");
+		$("#portfolioLink").addClass("active");
+	}
+	else{
+		$(".nav-item").removeClass("active");
+	}
+});
